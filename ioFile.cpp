0dc214bcc1f06655f52b163ea2416b0ef2a53bcb
neiranie <nurkhairanianneira@gmail.com>
2025-06-09 14:45:55 +0700

    code fix

--- ioFile.cpp
+++ ioFile.cpp
@@ -31,3 +31,23 @@ int main() {
     
     // membuat objek input file stream
     ifstream infile;
+    // membuka file yang dituliskan
+    infile.open("contohfile.txt");
+
+    cout << endl << ">= Membuka dan membaca file " << endl;
+    // jika file ada maka
+    if (infile.is_open())
+    {
+        // melakukan perulangan setiap baris
+        while (getline(infile, baris))
+        {
+            // dan tampilkan di sini
+            cout << baris << '\n';
+        }
+        // tutup file tersebut setelah selesai
+        infile.close();
+    }
+    // jika tidak ditemukan file maka akan menampilkan ini
+    else cout << "Unable to open file";
+    return 0;
+}

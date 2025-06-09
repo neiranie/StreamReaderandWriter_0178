6faef0f1326de9b24712b7da0fc89f3e6f1b1a40
neiranie <nurkhairanianneira@gmail.com>
2025-06-09 14:25:09 +0700

    membuat objek output file stream

---
+++ ioFile.cpp
@@ -0,0 +1,15 @@
+//StreamReaderandWriter_4NIMBelakang
+
+
+#include <fstream>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string baris;
+
+    // membuat objek output file stream.
+    ofstream outfile;
+
+}
